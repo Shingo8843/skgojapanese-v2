4c6fe7308c0d711fa1c24b4f3202caa21827f8ad
Shingo <shingo.sdsu@gmail.com>
2025-04-01 10:40:12 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: "https",
-          hostname: "skgo-resources2025.s3.us-west-1.amazonaws.com",
-          pathname: "/**",
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  // output: 'export', // Enables static export for S3 hosting
+  images: {
+    // unoptimized: true, // Disables Next.js image optimization (needed for static hosting)
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "skgo-resources2025.s3.us-west-1.amazonaws.com",
+        pathname: "/**",
+      },
+    ],
+  },
+  trailingSlash: true, // Ensures all routes have trailing slashes (useful for S3 static hosting)
+};
+
+export default nextConfig;
